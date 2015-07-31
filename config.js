1a005da6f643823f38461230ebfca896c00409cd
fuyun.zhao <fuyun.zhao@yeepay.com>
2015-07-31 16:44:27 +0800

    优化dataPath的默认值

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
+var path = require('path');
+
+var defaultDataPath = path.join(__dirname, 'data/');
+
 module.exports = {
 	// @link http://docs.mongodb.org/manual/reference/connection-string/
 	database: 'mongodb://localhost/febu',
 
-	// 存放部署数据的目录
-	dataPath: 'data/',
+	// 存放部署数据的目录，请填写绝对路径
+	dataPath: defaultDataPath,
 
 	// 约定requirejs项目的目录结构
 	// 示例 @link https://github.com/requirejs/example-multipage
