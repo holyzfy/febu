fd42df79c8b3c4dc3cb1701621f6f4c2a3d3bd01
‘fuyun.zhao’ <zhaofuyun202@gmail.com>
2015-08-11 12:30:59 +0800

    创建配置项示例文件

---
+++ config_sample.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+var defaultDataPath = path.join(__dirname, 'data/');
+
+module.exports = {
+	// @link http://docs.mongodb.org/manual/reference/connection-string/
+	database: 'mongodb://localhost/febu',
+
+	// 存放部署数据的目录，请填写绝对路径
+	dataPath: defaultDataPath,
+
+	// 约定requirejs项目的目录结构
+	// 示例 @link https://github.com/requirejs/example-multipage
+	amd: {
+		tools: 'tools', // 构建目录
+		optimizer: 'r.js', // 构建脚本
+		config: 'build.js', // 构建用的配置文件
+		www: 'www', // 项目目录
+		build: 'build' // 构建后的目录
+	}
+};
