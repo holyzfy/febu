bb4e09cb72347f183cdfeb899afbc473cdd66398
zhaofuyun <zhaofuyun202@gmail.com>
2018-07-12 16:27:23 +0800

    feat: 修正发布分支时，未能正常更新的问题

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,8 @@ gulp.task('before', done => {
 		clone,
         git.checkout.bind(git, '.'), // 确保本地仓库是干净的
         git.exec.bind(git, 'fetch', ['--all']),
-		git.checkout.bind(git, project.branch)
+		git.checkout.bind(git, project.branch),
+        git.exec.bind(git, 'pull')
 	];
 	async.series(tasks, err => {
 		clearTimeout(timer);
